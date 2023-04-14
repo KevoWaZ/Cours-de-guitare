1b92cf25b23b55924b7bad1e94866ae6fc172649
KevoWaZ <kevin.gauthier145@gmail.com>
2023-04-14 22:52:26 +0200

    l

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('.liste-nav')
 const btn = document.querySelector('.btn-toggle')
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     menu.classList.toggle('active-menu')
 })
 
@@ -9,7 +9,7 @@ const allLinks = document.querySelectorAll('.item-nav');
 
 allLinks.forEach(item => {
     item.addEventListener('click', () => {
-        menu.classList.toggle('active-menu') 
+        menu.classList.toggle('active-menu')
     })
 })
 
@@ -17,17 +17,17 @@ const change = document.querySelector(".btn");
 let cssEnabled = true; // état initial du CSS
 
 change.addEventListener('click', () => {
-  if (cssEnabled) { // si le CSS est actuellement activé
-    document.querySelectorAll('style,link[rel="stylesheet"]').forEach(item => item.remove()); // supprime tous les éléments <style> et <link>
-    change.textContent = "Réactiver le CSS"; // change le texte du bouton
-    cssEnabled = false; // met à jour l'état du CSS
-  } else { // si le CSS est actuellement désactivé
-    const head = document.querySelector("head");
-    const link = document.createElement("link"); // crée un élément <link>
-    link.rel = "stylesheet"; // définit le type d'élément <link> comme étant un fichier CSS
-    link.href = "style.css"; // définit le chemin du fichier CSS
-    head.appendChild(link); // ajoute l'élément <link> à la balise <head>
-    change.textContent = "Désactiver le CSS"; // change le texte du bouton
-    cssEnabled = true; // met à jour l'état du CSS
-  }
+    if (cssEnabled) { // si le CSS est actuellement activé
+        document.querySelectorAll('style,link[rel="stylesheet"]').forEach(item => item.remove()); // supprime tous les éléments <style> et <link>
+        change.textContent = "Réactiver le CSS"; // change le texte du bouton
+        cssEnabled = false; // met à jour l'état du CSS
+    } else { // si le CSS est actuellement désactivé
+        const head = document.querySelector("head");
+        const link = document.createElement("link"); // crée un élément <link>
+        link.rel = "stylesheet"; // définit le type d'élément <link> comme étant un fichier CSS
+        link.href = "style.css"; // définit le chemin du fichier CSS
+        head.appendChild(link); // ajoute l'élément <link> à la balise <head>
+        change.textContent = "Désactiver le CSS"; // change le texte du bouton
+        cssEnabled = true; // met à jour l'état du CSS
+    }
 });
